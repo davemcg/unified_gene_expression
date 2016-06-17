75e20c5565fd43a12d0213bdc24dd726c5647df0
davemcg <mcgaughey@gmail.com>
2016-06-17 15:05:06 -0400

    updated code and added E-MTAB-513

--- metadata_to_sqlite.R
+++ metadata_to_sqlite.R
@@ -10,13 +10,14 @@ library(stringr)
 # open sql database
 con <- dbConnect(RSQLite::SQLite(), "metaData.sqlite")
 
-arrayExpress_accessions <- c("E-GEOD-22765","E-GEOD-36695","E-GEOD-40524")
+# add to this vector as you bring in more experiments
+arrayExpress_accessions <- c("E-GEOD-22765","E-GEOD-36695","E-GEOD-40524","E-MTAB-513")
   # E-MTAB-4377 not here because: 
     # this one had to be modified slightly to make fake "Source Name" match up with the main table
     # I hope this to be a rare case, and thus can fully automate the import 
     # manually add E-MTAB-4377
-    open("E_MTAB_4377_metaData.Rdata")
-    dbWriteTable(con,"E_MTAB_4377",e.mtab.4377)
+    # open("E_MTAB_4377_metaData.Rdata")
+    # dbWriteTable(con,"E_MTAB_4377",e.mtab.4377)
 for(i in arrayExpress_accessions){
   base_url <- "https://www.ebi.ac.uk/arrayexpress/files/"
   end_url <- c(i,"/",i,".sdrf.txt")
@@ -41,24 +42,39 @@ for(i in arrayExpress_accessions){
   } else{print(paste(db_name,"already present, skipping!"))}
 }
 
+# example code to semi-automate editing of new core_data info 
+# first get current core_data
+core_data <- dbReadTable(con,"core_data")
+# then handfill in accesions for ArrayExpress, GEO, and ENA
+ae_accession <- "E_MTAB_513"  # hand-fill
+geo_accession <- "GSE30611"   # hand-fill
+erp_accession <- "ERP000546"  # hand-fill
+# Pull out arrayExpress info in to data frame
+arrayExpressTable <- dbReadTable(con,ae_accession)
+# build URL to get ENA info that "core_data" is built off of
+url <- paste(c("http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=",erp_accession,"&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,tax_id,scientific_name,instrument_model,library_layout,fastq_ftp,fastq_galaxy,submitted_ftp,submitted_galaxy,cram_index_ftp,cram_index_galaxy&download=txt"),collapse='')
+new_core_data <- fread(url)
+new_core_data$ArrayExpressAccession <- ae_accession
+new_core_data$geo <- geo_accession
+# move some arrayExpressTable info over to core_data
+########## will have to tweak depending on how scientists filled in metadata
+arrayExpressTable <- arrayExpressTable %>% select(Comment_ENA_EXPERIMENT, Characteristics_organism_part)
+colnames(arrayExpressTable) <- c("experiment_accession","Tissue")
+arrayExpressTable$Tissue_Source <- arrayExpressTable$Tissue
+# do a (left) join to ensure rows match up
+new_core_data <-left_join(data.table(arrayExpressTable),new_core_data,by = setNames("experiment_accession","experiment_accession"))
+new_core_data$notes <- NA # don't have notes for this one
+new_core_data$Name <- new_core_data$experiment_accession
+# CUSTOM FOR THIS EXPERIMENT. DON'T WANT SINGLE-END DATA
+new_core_data <- new_core_data %>% filter(library_layout == "PAIRED")
+# OK, now get columns in order
+new_core_data <- new_core_data %>% select(run_accession,ArrayExpressAccession,geo,study_accession:secondary_sample_accession,experiment_accession,tax_id:cram_index_galaxy,notes,Name,Tissue,Tissue_Source)
+new_core_data <- rbind(core_data,new_core_data)
+# OK, ready to update the table
+# FIRST HAND CHECK
+dbWriteTable(con,"core_data",data.frame(new_core_data),overwrite=TRUE)
 
-open("core_data.Rdata")
-# the above is a hand massaged info pulled from EBI ENA Study "Read File" info.
-  # see notes for my comments
-  # I also hand add the GEO ID and ArrayExpress Accession
-# Example for e.geod.36695: 
-# http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=SRP011895&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,tax_id,scientific_name,instrument_model,library_layout,fastq_ftp,fastq_galaxy,submitted_ftp,submitted_galaxy,cram_index_ftp,cram_index_galaxy&download=txt
-# can also use fread to pull in
-# not sure how to quickly do this, since the SRPXXXXXXX is not in the array expressed metadata, unfortunately
-# so for now, just have to use the website
-
-# manually add core_data table
-# can either drop and re-add in the future as I add more experiments
-# which isn't ideal, but easier
-# or do sql queries
-dbWriteTable(con,"core_data",core_data)
-
-
+#################
 # example queries
 # print out core_data, which holds the core info (ena run info) for the R plotting
 # more granular info comes from the individual tables for each experiment
