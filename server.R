e235bc811c9c676eae5fb89f463d22597fa1e004
davemcg <mcgaughey@gmail.com>
2016-06-30 15:32:56 -0400

    new files, extra plots

--- server.R
+++ server.R
@@ -4,8 +4,9 @@
 
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
-load('metaData_with_lengthScaledTPM.Rdata')
+load('gene_plotting_data_shiny.Rdata')
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
@@ -16,17 +17,18 @@ shinyServer(function(input, output) {
   #     when inputs change
   #  2) Its output type is a plot
   
-  output$distPlot <- renderPlot({
+  output$boxPlot <- renderPlot({
     
     gene <- input$Gene
-    data <- subset(metaData_lSTPM,Gene==gene)  
+    data <- metaData_lSTPM %>% filter(Gene %in% gene)  
     # draw the histogram with the specified number of bins
-    p<-ggplot(data=data,aes(x=Library_Name_s,y=log2(value+1),colour=tissue_s)) + 
-      geom_point() + facet_grid(~SRA_Study_s,space='free',scales='free') + 
-      theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-      ylab("Gene Expression | log2(lengthScaledTPM+1) ")
+    p<-ggplot(data=data,aes(x=Tissue,y=log2(lsTPM+1),colour=EyeMarker)) + 
+      geom_jitter(aes(shape=ProjectID),size=2) + geom_boxplot(alpha=0.5) + facet_wrap(~Gene,ncol=1) +
+      theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      ylab("Gene Expression | log2(lengthScaledTPM+1) ") +
+      scale_shape_manual(values=1:8)
     
     print(p)
     #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
+  }, height=700)
 })
