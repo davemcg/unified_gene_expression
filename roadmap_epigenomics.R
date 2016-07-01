2bf47b6b45c992459a647f8fa34ac2e306e43103
davemcg <mcgaughey@gmail.com>
2016-07-01 17:02:30 -0400

    tweaks to bring in roadmap epigenomics data with new main_data_db_pull.R

--- roadmap_epigenomics.R
+++ roadmap_epigenomics.R
@@ -4,12 +4,14 @@
 
 library(RSQLite)
 library(stringr)
-
+library(httr)
 # connect to full SRA db
+# this HUGE file is created with download_SRAdb.R
+# change your sqlite filename to match your creation (date will be the day you run download_SRAdb.R)
 sra_con<- dbConnect(SQLite(), '2016-06-27.SRAmetadb.sqlite')
 
 # connect to my db
-uge_con <- dbConnect(SQLite(), 'metaData.sqlite')
+# uge_con <- dbConnect(SQLite(), 'metaData.sqlite')
 
 # load up roadmap_epigenomics metadata
 # http://www.ncbi.nlm.nih.gov/geo/roadmap/epigenomics/
@@ -25,33 +27,39 @@ grab$experiment_accession <-
   sapply(grab$`SRA FTP`,function(x) strsplit(x,'\\/')[[1]][11])
 
 
-# grab SRAdb metadata to merge into core_data sqlite3
-sra_fields <- c('experiment_accession','sample_alias', 'submission_accession', 'run_accession','library_layout','instrument_model')
-run_info <- read.table(text='',col.names = sra_fields)
+# grab SRAdb metadata to merge into main table in sqlite3 database
+ALL_sra_fields <- colnames(dbGetQuery(sra_con,"SELECT * FROM sra WHERE experiment_accession='SRX023777'"))
+main_sra_fields <- c('experiment_accession','sample_alias', 'submission_accession', 'run_accession','library_layout','instrument_model')
+# initialize empty data frame
+main_run_info <- read.table(text='',col.names = main_sra_fields)
+run_info <- read.table(text='',col.names = ALL_sra_fields)
 for (srx in grab$experiment_accession) {
   fields <- 
+  # sql SELECT WHERE query that grab select sra columns for the roadmap epigenetics experiment accessions
   query <- paste(c("SELECT experiment_accession, sample_alias, submission_accession, run_accession, library_layout, instrument_model FROM sra WHERE experiment_accession='",srx,"'"),collapse='')
   data <- dbGetQuery(sra_con, query)
-  run_info <- rbind(run_info,data)
+  main_run_info <- rbind(main_run_info,data)
+  # now redo and grab all info
+  query <- paste(c("SELECT * FROM sra WHERE experiment_accession='",srx,"'"),collapse='')
+  data <- dbGetQuery(sra_con, query)
+  run_info <- rbind(run_info, data)
 }
-roadmap_core <- left_join(run_info,grab)
 
-# only keep the paired-end runs. 
-roadmap_core <- roadmap_core %>% filter(grepl("PAIRED",library_layout))
+roadmap_main <- left_join(main_run_info[,!names(main_run_info) %in% c("instrument_model")],grab[,c("experiment_accession","Sample Name")])
+
 
 # build fastq link from accession
 # http://www.ebi.ac.uk/ena/browse/read-download
 # explains how to build EBI fastq links from SRR/ERR
 # Have SRX with the roadmap_epignomics
-
-ebi_fastq_link_builder <- function(srr) {
+ebi_fastq_PE_link_builder <- function(srr) {
   front <- rep('ftp.sra.ebi.ac.uk/vol1/fastq/')
   end_1 <- paste(c(srr,'/',srr,'_1.fastq.gz'),collapse='')
-  end_2 <- paste(c(srr,'/',srr,'_2.fastq.gz'),collapse='')
+  #end_2 <- paste(c(srr,'/',srr,'_2.fastq.gz'),collapse='')
   if (nchar(srr) == 9) {
     middle <- paste(substr(srr,1,6),'/',sep='')
     link1 <- paste(c(front,middle,end_1),collapse='')
-    link2 <- paste(c(front,middle,end_2),collapse='')
+    #link2 <- paste(c(front,middle,end_2),collapse='')
   }
   else {
     middle <- paste(substr(srr,1,6),'/',sep='')
@@ -59,15 +67,64 @@ ebi_fastq_link_builder <- function(srr) {
     build_next_middle <- substr(srr,nchar(srr)-srr_end_to_grab,nchar(srr))
     build_next_middle <- str_sub(paste('00000',build_next_middle,sep=''),-3)
     link1 <- paste(c(front,middle,build_next_middle,'/',end_1),collapse='')
-    link2 <- paste(c(front,middle,build_next_middle,'/',end_2),collapse='')
+    #link2 <- paste(c(front,middle,build_next_middle,'/',end_2),collapse='')
   }
-  out <- paste(link1,link2,sep=';')
-  return(out)
+  #out <- paste(link1,link2,sep=';')
+  return(link1)
 }
 
-roadmap_core$fastq_ftp <- sapply(roadmap_core$run_accession, function(x) ebi_fastq_link_builder(x))
+fastq_urls <- ''
+for (srr in roadmap_main$run_accession){
+  if (try(!http_error(ebi_fastq_PE_link_builder(srr)),TRUE) == TRUE) {
+    fastq_urls <- c(fastq_urls, ebi_fastq_PE_link_builder(srr))
+    print(c(srr,ebi_fastq_PE_link_builder(srr)))
+  }
+  else {
+    link <- ebi_fastq_PE_link_builder(srr)
+    link <- gsub("_1.fastq.gz",".fastq.gz",link)
+    print(c(srr, link))
+    fastq_urls <- c(fastq_urls,link)
+  }
+}
+
+fastq_urls <- fastq_urls[-1]
+roadmap_main$comment_fastq_uri <- fastq_urls
+
+
+
+# rename columns to match "main" from grabbing arrayExpress info (main_data_db_pull.R)
+colnames(roadmap_main)[which(names(roadmap_main) == "experiment_accession")] <- "comment_ena_experiment"
+colnames(roadmap_main)[which(names(roadmap_main) == "sample_alias")] <- "source_name"
+colnames(roadmap_main)[which(names(roadmap_main) == "submission_accession")] <- "comment_ena_run"
+
+
+
+# only keep the paired-end runs. 
+roadmap_core <- roadmap_core %>% filter(grepl("PAIRED",library_layout))
+
+
+
+roadmap_core$fastq_ftp <- sapply(roadmap_core$run_accession, function(x) ebi_fastq_PE_link_builder(x))
 # high chance of this going pear-shaped next. make a copy
 hold <- roadmap_core
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # now need to reshape roadmap_core to match core_data from my database
 roadmap_core$ArrayExpressAccession <- 'Roadmap_Epigenomics'
