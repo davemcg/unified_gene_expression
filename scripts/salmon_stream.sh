24f178c228c7b4acaaaf9fb3a6c02e4c7c94c560
David McGaughey <mcgaughey@gmail.com>
2016-10-20 14:24:15 -0400

    rough handling for local and remote sra

--- scripts/salmon_stream.sh
+++ scripts/salmon_stream.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-module load samtools
+module load samtools/1.3.1
+module load sratoolkit/2.8.0
 
 srr_id=$1
 library=$2
 output=$3
 
-id=$srr_id
-
-echo $id $2 $3
-if [[ $srr_id =~ .*\.sra$ ]]; then id=${srr_id%.sra}; fi
-echo $id $2 $3
 
 if [[ "$library" == "paired" ]]; then
-	mkdir /scratch/mcgaugheyd/$id
-	sam-dump $srr_id | samtools view -bu - | \
-		samtools bam2fq - -1 /scratch/mcgaugheyd/$id/${id}_1.fastq -2 /scratch/mcgaugheyd/$id/${id}_2.fastq
 	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25/ \
 		--libType A --seqBias --gcBias \
-		-1 <(cat /scratch/mcgaugheyd/$id/${id}_1.fastq) \
-		-2 <(cat /scratch/mcgaugheyd/$id/${id}_2.fastq)  \
+		-1 <(sam-dump $srr_id | samtools view -bu - | samtools fastq - | grep '^@.*/1$' -A 3 --no-group-separator) \
+		-2 <(sam-dump $srr_id | samtools view -bu - | samtools fastq - | grep '^@.*/2$' -A 3 --no-group-separator)  \
 		-o $output \
 		-p $SLURM_CPUS_PER_TASK
 elif [[ "$library" == "single" ]]; then
 	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25/ \
         --libType A --seqBias --gcBias \
-		-r <(sam-dump $srr_id | samtools view -bu - | samtools bam2fq -) \
+		-r <(sam-dump $srr_id | samtools view -bu - | samtools fastq -) \
 		-o $output \
 		-p $SLURM_CPUS_PER_TASK  
 else
