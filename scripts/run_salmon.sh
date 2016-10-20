db8bc241faea6343aeb3c07dd373cb0fc01f92ef
David McGaughey <mcgaughey@gmail.com>
2016-10-20 14:21:49 -0400

    handles paired and single as well as gzip or not fastq input

--- scripts/run_salmon.sh
+++ scripts/run_salmon.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-fastq=$1
-paired=$2
-output=$3
+fastq_1=$1
+fastq_2=$2
+library=$3
+output=$4
 
-echo $1 $2 $3
+echo $1 $2 $3 $4
 
-if [["$paired" == "yes"]]; then
-	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCm38/salmon_0.7.2_gencode_v10 \
+if [[ "$library" == "paired" ]]; then
+	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25/ \
 		--libType A --seqBias --gcBias \
-		-1 <($fastq | zcat ) \
-		-2 <(${fastq%_1.fastq.gz}_2.fastq.gz | zcat )  \
+		-1 <(zcat -f $fastq_1) \
+		-2 <(zcat -f $fastq_2)  \
 		-o $output \
 		-p $SLURM_CPUS_PER_TASK
-elif [["$paired" == "no"]]; then
-	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCm38/salmon_0.7.2_gencode_v10 \
+elif [[ "$library" == "single" ]]; then
+	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25/ \
         --libType A --seqBias --gcBias \
-		-r <($fastq | zcat)
-		-o $output
+		-r <(zcat -f $fastq_1) \
+		-o $output \
 		-p $SLURM_CPUS_PER_TASK
 else
-	echo $paired "not 'yes' or 'no'"
+	echo $library "not 'paired' or 'single'"
 fi
