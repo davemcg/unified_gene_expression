4ae950d0abfea23d87271a61951c7b2a453df626
David McGaughey <mcgaughey@gmail.com>
2016-11-07 12:22:47 -0500

    added line for second index creation and fixed cd

--- scripts/create_salmon_index.sh
+++ scripts/create_salmon_index.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd /projects/mcgaugheyd/genome/GRCh38
+cd /data/mcgaugheyd/genomes/GRCh38
 
-~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon index --transcripts gencode.v25.pc_transcripts.fa.gz --index salmon_0.7.2_gencode_v25 --gencode --type quasi --perfectHash -p 16
+#~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon index --transcripts gencode.v25.pc_transcripts.fa.gz --index salmon_0.7.2_gencode_v25 --gencode --type quasi --perfectHash -p 16
+
+# second index with removal of low-use transcripts
+~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon index --transcripts gencode.v25.pc_transcripts.commonTx.fa.gz --index salmon_0.7.2_gencode_v25_commonTx --gencode --type quasi --perfectHash -p 32
