af170842cf61ad1eaf4ae232ccbdad1b2b322d4b
David McGaughey <mcgaughey@gmail.com>
2016-11-07 12:17:27 -0500

    added bootstraps and changed index

--- scripts/salmon_stream.sh
+++ scripts/salmon_stream.sh
@@ -8,15 +8,15 @@ output=$3
 file_fate=$4
 
 if [[ "$library" == "paired" ]]; then
-	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25/ \
-		--libType A --seqBias --gcBias \
+	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25_commonTx/ \
+		--libType A --seqBias --gcBias --numBootstraps 100 \
 		-1 <(sam-dump $srr_id | samtools view -bu - | samtools fastq - | grep '^@.*/1$' -A 3 --no-group-separator) \
 		-2 <(sam-dump $srr_id | samtools view -bu - | samtools fastq - | grep '^@.*/2$' -A 3 --no-group-separator)  \
 		-o $output \
 		-p $SLURM_CPUS_PER_TASK
 elif [[ "$library" == "single" ]]; then
-	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25/ \
-        --libType A --seqBias --gcBias \
+	~/bin/Salmon-0.7.2_linux_x86_64/bin/./salmon quant --index /data/mcgaugheyd/genomes/GRCh38/salmon_0.7.2_gencode_v25_commonTx/ \
+        --libType A --seqBias --gcBias --numBootstraps 100 \
 		-r <(sam-dump $srr_id | samtools view -bu - | samtools fastq -) \
 		-o $output \
 		-p $SLURM_CPUS_PER_TASK  
